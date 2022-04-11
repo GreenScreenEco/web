968bcff62d87d92a9f2eef6d838f8c52598e6fa3
Wolfizen <wolfizen@wolfizen.net>
2022-04-11 01:36:17 -0700

    Enable extension installation

--- components/InstallButton.tsx
+++ components/InstallButton.tsx
@@ -18,8 +18,8 @@ export default function InstallButton({style, className}: Props) {
   return (
     <div className={classNames(styles.container, className)}>
       <Button href="/install">Install GreenScreen</Button>
-      <img src="/firefox_logo_2019.svg" alt="Google Chrome Browser Logo" className={styles.logo}/>
-      <img src="/chrome_logo_2022.svg" alt="Firefox Browser Logo" className={styles.logo}/>
+      <img src="/firefox_logo_2019.svg" alt="Firefox Browser Logo" className={styles.logo}/>
+      {/*<img src="/chrome_logo_2022.svg" alt="Google Chrome Browser Logo" className={styles.logo}/>*/}
     </div>
   )
 }
